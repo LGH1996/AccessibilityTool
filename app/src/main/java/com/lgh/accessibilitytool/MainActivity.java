609d1f456bbf1effb329c14f95b06163f52a3367
LGH1996 <2281442260@qq.com>
2019-09-01 21:34:54 +0800

    -

--- app/src/main/java/com/lgh/accessibilitytool/MainActivity.java
+++ app/src/main/java/com/lgh/accessibilitytool/MainActivity.java
@@ -2,12 +2,13 @@ package com.lgh.accessibilitytool;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.provider.Settings;
 import android.widget.Toast;
 
@@ -19,8 +20,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try {
-
             Context context = getApplicationContext();
+            PackageManager packageManager = getPackageManager();
 
             if (MyAccessibilityService.handler != null) {
                 if (Settings.canDrawOverlays(context)) {
@@ -28,28 +29,36 @@ public class MainActivity extends Activity {
                     Toast.makeText(context, "请确保允许应用后台运行", Toast.LENGTH_SHORT).show();
                 }
             } else {
-                Toast.makeText(context, "请打开＜无障碍＞服务", Toast.LENGTH_SHORT).show();
                 Intent intent_abs = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
                 intent_abs.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent_abs);
-            }
-
-            if (!Settings.canDrawOverlays(context)) {
-                try {
-                    Intent intent_dol = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-                    intent_dol.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent_dol);
-                } catch (ActivityNotFoundException e) {
-                    openAppSet();
-                } finally {
-                    Toast.makeText(context, "请打开应用＜悬浮窗＞权限,并设置允许后台运行", Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(context, "请打开＜无障碍＞服务", Toast.LENGTH_SHORT).show();
             }
 
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 openAppSet();
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
                 Toast.makeText(context, "请授予＜读写手机存储＞权限，,并设置允许后台运行", Toast.LENGTH_SHORT).show();
+            }
+
+            if (!Settings.canDrawOverlays(context)) {
+                Intent intent_dol = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                intent_dol.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                ResolveInfo resolveInfo = packageManager.resolveActivity(intent_dol, PackageManager.MATCH_ALL);
+                if (resolveInfo != null) {
+                    startActivity(intent_dol);
+                } else {
+                    openAppSet();
+                }
+                Toast.makeText(context, "请打开应用＜悬浮窗＞权限,并设置允许后台运行", Toast.LENGTH_SHORT).show();
+            }
+
+            if (!((PowerManager) getSystemService(POWER_SERVICE)).isIgnoringBatteryOptimizations(getPackageName())) {
+                Intent intent_ibo = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + getPackageName()));
+                intent_ibo.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                ResolveInfo resolveInfo = packageManager.resolveActivity(intent_ibo, PackageManager.MATCH_ALL);
+                if (resolveInfo != null)
+                    startActivity(intent_ibo);
             }
 
         } catch (Throwable e) {
@@ -64,8 +73,8 @@ public class MainActivity extends Activity {
     }
 
     private void openAppSet() {
-        Intent intent_sto = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
-        intent_sto.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent_sto);
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
